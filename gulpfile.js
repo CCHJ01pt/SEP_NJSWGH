25aa6db3bd583aeb20a77759b432f1da5768b140
CCHJ01pt <chong_chiang_hui_jonathan01.pt@ichat.sp.edu.sg>
2021-04-25 13:08:48 +0800

    edited default message

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,40 @@
 "use strict";
 
-let gulp = require('gulp');
-let jshint = require('gulp-jshint');
-
-gulp.task('default',['jshint','test','serve']);
-
-gulp.task('jshint',()=>{
-	return gulp.src('./*.js')
-		.pipe(jshint())
-		.pipe(jshint.reporter('default'));
-})
-.task('test',()=>{
-	require('./test.js');
-})
-.task('serve',()=>{
-	require('./main.js');
-});
-
 // let gulp = require('gulp');
 // let jshint = require('gulp-jshint');
 
-// gulp.task('jshint', ()=>{
-// 	return gulp.src(['./*.js'])
-// 		.pipe(jshint())
-// 		.pipe(jshint.reporter('default'))
-// 		.pipe(gulp.dest('./'));
-// });
+// gulp.task('default',['jshint','test','serve']);
 
-// gulp.task('test',()=>{
+// gulp.task('jshint',()=>{
+// 	return gulp.src('./*.js')
+// 		.pipe(jshint())
+// 		.pipe(jshint.reporter('default'));
+// })
+// .task('test',()=>{
 // 	require('./test.js');
-// });
-// gulp.task('serve',()=>{
+// })
+// .task('serve',()=>{
 // 	require('./main.js');
 // });
-// gulp.task('default', gulp.series(
-// 	gulp.parallel(
-// 		'jshint','serve','test'
-		// ()=>{
-		// 	return gulp.src(['./*.js'])
-		// 	.pipe(jshint())
-		// 	.pipe(jshint.reporter('default'));
-		// }
-// 	)
-// ));
+
+let {src, task, series, parallel} = require('gulp');
+let jshint = require('gulp-jshint');
+
+task('test',(done)=>{
+	require('./test.js');
+	done();
+});
+task('serve',(done) => {
+	require('./main.js');
+	done();
+});
+task('default', series(
+		(done) => {
+			return src(['./*.js'])
+			.pipe(jshint())
+			.pipe(jshint.reporter('default'));
+			
+			done();
+		}
+		,'serve','test'
+));
