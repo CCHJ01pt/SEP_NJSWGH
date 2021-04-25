a4481c08bb972e98bd7a5f49d780fd03153ca237
CCHJ01pt <chong_chiang_hui_jonathan01.pt@ichat.sp.edu.sg>
2021-04-25 16:26:04 +0800

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,24 @@
 "use strict";
 
-// let gulp = require('gulp');
-// let jshint = require('gulp-jshint');
-
-// gulp.task('default',['jshint','test','serve']);
-
-// gulp.task('jshint',()=>{
-// 	return gulp.src('./*.js')
-// 		.pipe(jshint())
-// 		.pipe(jshint.reporter('default'));
-// })
-// .task('test',()=>{
-// 	require('./test.js');
-// })
-// .task('serve',()=>{
-// 	require('./main.js');
-// });
-
-let {src, task, series} = require('gulp');
+let gulp = require('gulp');
 let jshint = require('gulp-jshint');
 
-task('test',(done)=>{
+gulp.task('jshint',(done)=>{
+	return gulp.src('./*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('default'));
+
+		done();
+});
+gulp.task('test',(done)=>{
 	require('./test.js');
 
 	done();
 });
-task('serve',(done) => {
+gulp.task('serve',(done)=>{
 	require('./main.js');
 
 	done();
 });
-task('default', series(
-		(done) => {
-			return src(['./*.js'])
-			.pipe(jshint())
-			.pipe(jshint.reporter('default'));
-
-			done();
-		}
-		,'serve','test'
-));
+gulp.task('default', gulp.series(
+	'jshint','test','serve'));
